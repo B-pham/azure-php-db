be7f1fc6c054e2d7405bc33080d7c44a8aba0b67
B-pham <b.pham48@gmail.com>
2022-09-17 17:26:16 -0500

    trying something else

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     try {
         $conn = new PDO("sqlsrv:server = tcp:konnectvr-db.database.windows.net,1433; Database = konnectVR-Data", "konnectVR", "TZeu4kAmTK2BWPS");
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        print("connected to the server!");
     }
     catch (PDOException $e) {
         print("Error connecting to SQL Server.");
@@ -14,17 +15,32 @@
     $loginUser -> username = 'Bill';
     $loginPass -> Password = '123qwe';
 
-    try {
+    /*try {
         $loginResult = $conn -> query('SELECT password FROM loginData WHERE username = '. $loginUser .'');
         $loginResult-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         if($loginPass -> password = $loginResult){
-            echo "Password is correct!";
+            print("Password is correct!");
         }
     
     }
     catch(PDOException $e){
         print("Error finding username in database.");
         die(print_r($e));
+    }*/
+
+    $sql = "SELECT Password FROM login Where Username = '". $loginUser . "'";
+
+    $result = $conn->query($sql);
+
+    if($result -> num_rows > 0)
+    {
+        while($row = $result->fetch_assoc())
+        {
+            if($row["Password"] == $loginPass)
+            {echo "Sucessful Login and Connection!!";}
+        }
     }
+    else
+    {echo "Sucessful connection, but failed login";}
 
 ?>
