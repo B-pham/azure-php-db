6d3c69abb316a243a96d3d9c7f68f842f011d4cf
B-pham <b.pham48@gmail.com>
2022-09-16 20:15:33 -0500

    Smodge

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
     
     // PHP Data Objects(PDO) Sample Code:
     try {
-        $conn = new PDO("sqlsrv:server = tcp:konnectvr-db.database.windows.net,1433; Database = konnectVR-Data", "konnectVR", "{your_password_here}");
+        $conn = new PDO("sqlsrv:server = tcp:konnectvr-db.database.windows.net,1433; Database = konnectVR-Data", "konnectVR", "TZeu4kAmTK2BWPS");
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
     catch (PDOException $e) {
         print("Error connecting to SQL Server.");
         die(print_r($e));
-}
+    }
 
     
     
