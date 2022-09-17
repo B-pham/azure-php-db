00dd66264a59b48402700a71c0825b57b8a5a916
B-pham <b.pham48@gmail.com>
2022-09-16 19:44:18 -0500

    More errors Smodge

--- index.php
+++ index.php
@@ -10,17 +10,19 @@
     }
     
     // SQL Server Extension Sample Code:
-    $connectionInfo = array("UID" => "konnectVR", "pwd" => "TZeu4kAmTK2BWPS", "Database" => "konnectVR-Data", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-    $serverName = "tcp:konnectvr-db.database.windows.net,1433";
-    $conn = sqlsrv_connect($serverName, $connectionInfo);
-
-    $loginUser = $_POST["LoginUser"];
-    $loginPass = $_POST["LoginPass"];
-
-    $loginPass = $conn -> query('SELECT TOP(1)* FROM [dbo].[loginData]');
-    $loginPass -> setfetchmode(PDO::FETCH_ASSOC);
+    //$connectionInfo = array("UID" => "konnectVR", "pwd" => "TZeu4kAmTK2BWPS", "Database" => "konnectVR-Data", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+    //$serverName = "tcp:konnectvr-db.database.windows.net,1433";
+    //$conn = sqlsrv_connect($serverName, $connectionInfo);
     
     echo "Hello World! \n";
+    
+    //$loginUser = $_POST["LoginUser"];
+    //$loginPass = $_POST["LoginPass"];
+
+    //$loginPass = $conn -> query('SELECT TOP(1)* FROM [dbo].[loginData]');
+    //$loginPass -> setfetchmode(PDO::FETCH_ASSOC);
+    
+    
     //echo "$conn";
 
 ?>
