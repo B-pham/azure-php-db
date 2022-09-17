262ab555169c2626fa32e1776cde91d14fa14ba4
B-pham <b.pham48@gmail.com>
2022-09-16 20:13:06 -0500

    Smodging

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <?php  
     print("Hello World!");
     
-    //SQL Server Extension Sample Code:
-    $connectionInfo = array("UID" => "konnectVR", "pwd" => "TZeu4kAmTK2BWPS", "Database" => "konnectVR-Data", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-    $serverName = "tcp:konnectvr-db.database.windows.net,1433";
-    $conn = sqlsrv_connect($serverName, $connectionInfo);
+    // PHP Data Objects(PDO) Sample Code:
+    try {
+        $conn = new PDO("sqlsrv:server = tcp:konnectvr-db.database.windows.net,1433; Database = konnectVR-Data", "konnectVR", "{your_password_here}");
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch (PDOException $e) {
+        print("Error connecting to SQL Server.");
+        die(print_r($e));
+}
+
     
     
     
