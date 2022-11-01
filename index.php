8cc380ea90661ff2314a77298a58477c3b1bb4fd
JustinVangg <pure_vaj16@hotmail.com>
2022-11-01 13:50:56 -0500

    Update

--- index.php
+++ index.php
@@ -1,63 +1,16 @@
-<?php  
-    print("Hello World!". "<br>");
-    
-    // PHP Data Objects(PDO) Sample Code:
-    try {
-        $conn = new PDO("sqlsrv:server = tcp:konnectvr-db.database.windows.net,1433; Database = konnectVR-Data", "konnectVR", "TZeu4kAmTK2BWPS");
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        print("connected to the server!". "<br>");
-    }
-    catch (PDOException $e) {
-        print("Error connecting to SQL Server.");
-        die(print_r($e));
-    }
+<?php
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:konnectvr-db.database.windows.net,1433; Database = konnectVR-Data", "konnectVR", "TZeu4kAmTK2BWPS");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
 
-    $loginUser = $_POST["LoginUser"];
-    $loginPass = $_POST["LoginPass"];
-
-    /*D = "3"
-    $username = "Justin";
-    $password = "1234abc";
-    $accessCode = "1993";
-    */
-
-    try {
-        $sql = "SELECT password FROM loginData WHERE username = '". $loginUser ."'";
-        $temp = $conn -> query($sql);//Grab inforamtion based on above query statement
-        $loginResult = $temp->fetch(PDO::FETCH_ASSOC);//Sort rows into arrays
-        //$loginResult-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        if($loginPass == $loginResult['password']){//Check array against entered info
-            print("Password is correct!". "<br>");
-        }
-    }
-
-    catch(PDOException $e){
-        print("Error finding username in database.");
-        die(print_r($e));
-    }
-
-   /*sql = "INSERT INTO loginData (iD, username, password, accessCode)
-        Values ('".$iD."', '".$username."', '".$password."', '".$accessCode."')";
-    $result = mysqli_query($conn, $sql);
-
-    if(!result) echo "Error";
-    else echo "Works great!";
-    */
-
-
-    /*$sql = "SELECT Password FROM login Where Username = '". $loginUser . "'";
-
-    $result = $conn->query($sql);
-
-    if($result -> num_rows > 0)
-    {
-        while($row = $result->fetch_assoc())
-        {
-            if($row["Password"] == $loginPass)
-            {echo "Sucessful Login and Connection!!";}
-        }
-    }
-    else
-    {echo "Sucessful connection, but failed login.";}*/
-
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "konnectVR", "pwd" => "TZeu4kAmTK2BWPS", "Database" => "konnectVR-Data", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:konnectvr-db.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
