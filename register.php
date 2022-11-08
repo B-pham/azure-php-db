4dc304c2e8b021fa70504eaa0d16d2885eb5f9be
JustinVangg <pure_vaj16@hotmail.com>
2022-11-08 01:50:44 -0600

    Update Register.php

--- register.php
+++ register.php
@@ -1,18 +1,17 @@
 <?php  
-
-    print("Hello World!". "<br>");
-    
     // PHP Data Objects(PDO) Sample Code:
     try {
         $conn = new PDO("sqlsrv:server = tcp:konnectvr-db.database.windows.net,1433; Database = konnectVR-Data", "konnectVR", "TZeu4kAmTK2BWPS");
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        print("connected to the server!". "<br>");
+        print("Connected to the server!". "<br>");
     }
     catch (PDOException $e) {
         print("Error connecting to SQL Server.");
         die(print_r($e));
     }
 
+
+    //Insert Data into Database
     $email = $_POST["emailPost"];
     $password = $_POST["passwordPost"];
     $accessCode = $_POST["accessCodePost"];
@@ -20,11 +19,8 @@
     try {
         $sql = "INSERT INTO loginData (email, password, accessCode)
             Values('".$email."', '".$password."', '".$accessCode."')";
-        if($conn->query($sql) === TRUE){
-            echo "Account Created!";
-        } else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
-        }
+        echo "Account Created in database!";
+        echo "SQL Command: " . $sql . "<br>";
     }
 
     catch(PDOException $e){
