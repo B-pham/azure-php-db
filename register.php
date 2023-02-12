1a0a2eb9c6d9be7692287d13c04ed29aadc6c215
B-pham <b.pham48@gmail.com>
2023-02-12 07:18:58 -0600

    Connection string changes

--- register.php
+++ register.php
@@ -1,8 +1,9 @@
 <?php  
     // PHP Data Objects(PDO) Sample Code:
     try {
-        $conn = new PDO("sqlsrv:server = tcp:konnectvr.database.windows.net,1433; Database = KVR_Database", "CloudSAf20f247f", " Konnectvr2023");
+        $conn = new PDO("sqlsrv:server = tcp:konnectvr.database.windows.net,1433; Database = KVR_Database", "CloudSAf20f247f", "Konnectvr2023");
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        //print("Connected to the server!". "<br>");
     }
     catch (PDOException $e) {
         print("Error connecting to SQL Server.");
