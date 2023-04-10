c54735d9ced313e45f5b92826f2e6a901d13fdbd
JustinVangg <pure_vaj16@hotmail.com>
2023-04-09 23:47:49 -0500

    Update callLogin.php

--- App/callLogin.php
+++ App/callLogin.php
@@ -4,8 +4,8 @@
     $email = $_POST["emailPOST"];
     $password = $_POST["passwordPOST"];
 
+    $login = new Login();
     try{
-        $login = new Login();
         print($login -> LoginCheck($email, $password));
     }catch(PDOException $e){
         print("Error finding username in database.");
