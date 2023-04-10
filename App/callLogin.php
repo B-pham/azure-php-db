4b3f55954ebfc92c9aedfd71e8698b32b6c18208
B-pham <59837130+B-pham@users.noreply.github.com>
2023-04-10 20:46:29 +0900

    bugfix?

--- App/callLogin.php
+++ App/callLogin.php
@@ -1,12 +1,14 @@
 <?php
-    namespace app;
+    namespace App;
     
     $email = $_POST["emailPOST"];
     $password = $_POST["passwordPOST"];
 
-    $login = new Login();
+    
     try{
-        print($login -> LoginCheck($email, $password));
+        $login = new Login();
+        $loginMessage = $login -> LoginCheck($email, $password);
+        print($loginMessage);
     }catch(PDOException $e){
         print("Error finding username in database.");
         die(print_r($e));
