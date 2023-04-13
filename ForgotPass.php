be9ef9c769fbc17c2ec1f4fdc5cfc2efcfe51ed0
JustinVangg <pure_vaj16@hotmail.com>
2023-04-12 21:33:14 -0500

    Update ForgotPass.php

--- ForgotPass.php
+++ ForgotPass.php
@@ -34,8 +34,8 @@
                 $mail->isSMTP();
                 $mail->Host = 'smtp.gmail.com';//Server emails are sent from
                 $mail->SMTPAuth = true;
-                $mail->Username = 'kvrtesting02@gmail.com';//Email address that sends the email
-                    $mail->Password = 'wazngfpibmkeroch';//App password for the gmail account
+                $mail->Username = 'konnectvr@gmail.com';//Email address that sends the email
+                    $mail->Password = 'widjbyeskgtypysv';//App password for the gmail account
                 $mail->SMTPSecure = 'tls';
                 $mail->SMTPOptions = array('ssl' => array(//Needed to connect to server, however this in of itself is a security flaw
                                                             'verify_peer'=>false,
@@ -44,7 +44,7 @@
                                                         )
                                           );
                 $mail->Port = 587;
-                $mail->setFrom('kvrtesting02@gmail.com');
+                $mail->setFrom('konnectvr@gmail.com');
                 $mail->addAddress($to);
                 $mail->isHTML(true);
                 $mail->Subject = $subject;
