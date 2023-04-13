630a854a4a30ae6fe28d8e4edf46ea6dccbfc1c3
JustinVangg <pure_vaj16@hotmail.com>
2023-04-13 00:51:40 -0500

    Update Login.php

--- App/Login.php
+++ App/Login.php
@@ -32,8 +32,7 @@
                 } else if(password_verify($password, $stored)){//Check array against entered info
                     return("Password is correct!");
                 } else{
-                    return("Password is incorrect. Please try again with another email or password. \n");
-                    return("Stored: $store Password:$password");
+                    return("Password is incorrect. Please try again with another email or password. <br/> Stored: $store Password:$password");
                     //print(".$stored. , .$password.");
                 }
                     
