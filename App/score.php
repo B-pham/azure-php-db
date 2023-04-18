13650400fd7ee9b37193f0baeb84214fc78bb80d
JustinVangg <pure_vaj16@hotmail.com>
2023-04-17 20:02:22 -0500

    Update Score.php

--- App/score.php
+++ App/score.php
@@ -56,7 +56,7 @@
                     $mail->Subject = $subject;
                     //$to = $result['Email'];
                     $message = " A student has completed the following quiz: <br/><br/> 
-                                Student: $user <br/> Score: $score <br/> Test: $test <br/> <br/> <br/>
+                                Student Name: $user <br/> Score: $score <br/> Test Name: $test <br/> <br/> <br/>
                                 This is an automated message, please do not reply.";
                     $mail->Body = $message;
                     $mail->send();
